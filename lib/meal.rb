21a8c036cf5e7c8c60d8abed214d8f765d4b5ecd
kain <kaindoneson@gmail.com>
2020-08-06 15:35:17 -0400

    its all making sense now but the tests are really helping me

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,17 @@
 class Meal
+    attr_accessor :customer, :waiter, :total, :tip
+    @@all = [];
 
+    def initialize(custy, waity, toty, tip) #im being so lazy loooool
+        @customer = custy;
+        @waiter = waity;
+        @total = toty;
+        @tip = tip;
+        
+        @@all.push(self);
+    end
+
+    def self.all
+        @@all;
+    end
 end
