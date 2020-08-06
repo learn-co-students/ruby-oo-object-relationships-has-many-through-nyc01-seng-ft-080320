60e2998711c3dd1bdc42751d9331c39198db66d1
AmSmo <adam.smolenski@gmail.com>
2020-08-06 12:03:06 -0400

    phew

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class Customer
   
+    attr_accessor :name, :age
+    
+    @@all = []
+    @@meals = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name,age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def new_meal(waiter,total,tip)
+        tasty = Meal.new(waiter, self, total, tip)
+        @@meals << tasty
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.customer == self}
+    end
+
+    def waiters
+        self.meals.map {|meal| meal.waiter}
+    end
+    
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,32 @@
 class Waiter
 
+    attr_accessor :name, :years_experience
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name, years_experience)
+        @name = name
+        @years_experience = years_experience
+        @@all << self
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals   
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        meal_instance = meals.max_by do |a|
+            a.tip
+        end
+        meal_instance.customer
+    end
+
 end
