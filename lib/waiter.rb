142a858f0579640ec82adc9b4ccfc3f98a8c331a
royFloresNyc <royfloresnyc@gmail.com>
2020-08-06 16:32:58 -0400

    Object associations in Waiter class

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -13,4 +13,17 @@ class Waiter
     @@all
   end 
   
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.filter { |meal| meal.waiter == self}
+  end 
+
+  def best_tipper
+    best_tipped_meal = self.meals.max{ |meal_a , meal_b| meal_a.tip <=> meal_b.tip}
+    best_tipped_meal.customer 
+  end 
+
 end
