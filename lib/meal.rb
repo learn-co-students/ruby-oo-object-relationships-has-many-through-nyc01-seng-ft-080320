af75f070ef05ba77fe0b59582535204090c01af6
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-08-08 21:12:22 -0400

    finished lab

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,30 @@
 class Meal
+ 
+attr_accessor :waiter, :customer, :total, :tip
+   
+@@all = []
+@@tip = []
+   
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+    @@tip << Array[tip, waiter, customer]
+  end
+   
+  def self.all
+    @@all
+  end
 
-end
+  def self.all_tips
+    @@tip
+  end
+    
+   def new_meal(waiter, total, tip=0)
+      Meal.new(waiter, self, total, tip)
+    end
+  
+
+  end
