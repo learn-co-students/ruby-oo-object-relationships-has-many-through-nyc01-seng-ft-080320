1a99dbea5ea641b2881cff5d794fad3f7935a2d6
D1yr <alexander.m.sahli@gmail.com>
2020-08-11 13:36:08 -0400

    stuck on getting new_meal working

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,15 @@
 class Customer
-  
+    attr_accessor :name, :age
+    @@all = []
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def new_meal(meal)
+    self.meal = Meal.new
+  end
 end
